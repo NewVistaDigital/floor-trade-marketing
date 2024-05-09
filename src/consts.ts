19d4f360d5f0ab629d23d4952c3b2970f522516b
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-05-09 13:21:38 -0600

    feat: add google analytics

--- src/consts.ts
+++ src/consts.ts
@@ -18,6 +18,6 @@ export const SITE_UTM_SOURCE = slugify(SITE_TITLE);
 
 export const UI_THEME = "light"; // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
 
-export const DISABLE_INDEXING = true;
+export const DISABLE_INDEXING = false;
 
-export const ANALYTICS_GA4 = "G-T100000000";
+export const ANALYTICS_GA4 = "G-M0JW39ZSCL";
