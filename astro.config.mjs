e36aa28fbcf4a013fe9506dccef59fc0a10f0917
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-05-10 08:08:59 -0600

    add partytown for google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
+import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -9,7 +10,18 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://floortrademarketing.com",
-  integrations: [mdx(), sitemap(), icon(), tailwind(), robotsTxt()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon(),
+    tailwind(),
+    robotsTxt(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    })
+  ],
   output: "static",
-  adapter: netlify(),
+  adapter: netlify()
 });
